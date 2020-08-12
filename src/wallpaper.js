fa0327d2723691c50ba090107a30c0fa2603c2aa
PortiaAba <69083305+PortiaAba@users.noreply.github.com>
2020-08-12 12:10:42 +0000

    third commit

---
+++ src/wallpaper.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/sample-wallpaper.css'
+
+function Wallpaper(props) {
+    return (
+
+            <article class="wallpaper">
+            <img src={props.pic} alt="wallpaper #1" class="wallpaper" />
+            </article>
+
+    )
+}
+
+export default Wallpaper
